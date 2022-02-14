ade76b0e196d39b096466f4f593fb41c146e33a4
Wasit Limprasert <wasit7@gmail.com>
2022-02-14 17:05:23 +0700

    after class week06

---
+++ week06/demo2/main.py
@@ -0,0 +1,10 @@
+#main.py
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello from host'
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0',port=8000)
