a3d16185fd332bb8784ba9b2edabbb8f15259d06
Wasit Limprasert <wasit7@gmail.com>
2022-04-04 15:49:04 +0700

    Move to postgresql

--- week12/myproject/myproject/settings.py
+++ week12/myproject/myproject/settings.py
@@ -74,11 +74,23 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': 'postgres',
+       'USER': 'postgres',
+       'PASSWORD': 'password',
+       'HOST': '127.0.0.1',
+       'PORT': '5432',
+   }
 }
 
 
